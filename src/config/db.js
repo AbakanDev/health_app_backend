da01b6d4b61c55e642c2c695e340ce8f325456b2
Abakan <abakandev@gmail.com>
2026-08-05 16:38:21 +0700

    remove ca.pem and fix db.js a little bit

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,4 @@
 const mysql = require('mysql2/promise');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -9,9 +7,6 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
-    ssl: {
-        ca: fs.readFileSync(path.join(__dirname, 'ca.pem'))
-    },
     timezone: '+07:00', 
     dateStrings: true,  
     waitForConnections: true,
